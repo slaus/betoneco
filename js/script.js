a190c4ec9a930052cbd10e39f809e10584990e0f
Vyacheslav <mister.slaus@gmail.com>
2018-04-05 16:32:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,97 @@
+//Scripts
+jQuery(function ($) {
+
+//Catalogue menu
+    $("#catalogue").click(function (e) {
+        e.preventDefault();
+    });
+
+    $(".plus").click(function () {
+        // $(".header-catalogue-menu-submenu").find(".submenu").hide();
+        $(this).siblings(".submenu").toggle();
+    });
+
+//Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+// scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    //Slider settings
+    var jcarousel = $('.jcarousel');
+
+    jcarousel
+        .jcarousel({
+            animation: {
+                duration: 1000,
+                speed: 1000,
+                easing: 'linear',
+                complete: function () {
+                }
+            }
+        })
+        .jcarouselAutoscroll({
+            interval: 5000,
+            target: '+=1',
+            autostart: true,
+        })
+        .on('mouseover', function (e) {
+            $(this).jcarouselAutoscroll('stop');
+        })
+        .on('mouseout', function (e) {
+            $(this).jcarouselAutoscroll('start');
+        });
+
+    jcarousel
+        .on('jcarousel:reload jcarousel:create', function () {
+            var carousel = $(this),
+                width = carousel.innerWidth();
+
+            carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+        })
+        .jcarousel({
+            wrap: 'circular'
+        });
+
+
+    $('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('active');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselPagination({
+            perPage: 1,
+            item: function (page) {
+                return '<a href="#' + page + '">' + page + '</a>';
+            }
+        });
+
+});
+
+
